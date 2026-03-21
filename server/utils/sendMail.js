d846ef908786e8a59a0f4a1cfeedc808569f792e
anti00700 <aniruddhtiwari594@gmail.com>
2026-03-21 10:48:45 +0530

    fix brevo sdk package name

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,11 +1,14 @@
-const Brevo = require("@getbrevo/brevo");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const sendMail = async (email, subject, data) => {
-  const apiInstance = new Brevo.TransactionalEmailsApi();
+  // Set up the API client
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+  const apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey = process.env.BREVO_API_KEY;
 
-  apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-  const sendSmtpEmail = new Brevo.SendSmtpEmail();
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
   sendSmtpEmail.sender = {
     name: "Batchit",
